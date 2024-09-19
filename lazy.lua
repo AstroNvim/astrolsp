ef6c46d9d37e6048c67eb9237a8fdb437cbf48ef
Micah Halter <micah@mehalter.com>
2024-09-19 07:50:56 -0400

    ci: add `lazy.lua` for configuring `opts_extend`

---
+++ lazy.lua
@@ -0,0 +1,9 @@
+return {
+  "AstroNvim/astrolsp",
+  opts_extend = {
+    "formatting.disabled",
+    "formatting.format_on_save.allow_filetypes",
+    "formatting.format_on_save.ignore_filetypes",
+    "servers",
+  },
+}
